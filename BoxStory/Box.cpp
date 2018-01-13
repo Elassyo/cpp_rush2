be568cf85602c2493fb7d895094b0eb7c8ce5790
alexandre <alexandre.debeaumont@epitech.eu>
2018-01-13 15:05:07 +0100

    Add BoxStory

---
+++ BoxStory/Box.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_rush2
+** File description:
+** Box.cpp
+*/
+
+#include "Box.hpp"
+
+Box::Box() : Wrap("Box")
+{
+}
+
+void Box::closeMe()
+{
+	this->_isOpen = false;
+}
---
+++ BoxStory/GiftPaper.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_rush2
+** File description:
+** GiftPaper.cpp
+*/
+
+#include "GiftPaper.hpp"
+
+GiftPaper::GiftPaper() : Wrap("GiftPaper")
+{
+}
